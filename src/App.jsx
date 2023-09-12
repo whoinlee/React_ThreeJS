22a882acd85c75758b4a55c70b77a0b725724b35
WhoIN Lee <whoinlee@gmail.com>
2023-09-12 19:55:42 -0400

    Jumbotron component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Jumbotron from './components/Jumbotron';
 import Nav from './components/Nav';
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <Nav />
+      <Jumbotron />
     </div>
   );
 }
---
+++ src/components/Jumbotron.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Iphone from "../assets/images/iphone-14.jpg";
+import HoldingIphone from "../assets/images/iphone-hand.png";
+
+const Jumbotron = () => {
+  return (
+    <div className="jumbotron-section wrapper">
+      <h2 className='title'>New </h2>
+      <img className='logo' src={Iphone} alt="iPhone 14 pro"></img>
+      <p className='text'>Big and bigger.</p>
+      <span className='description'>From $41.62/mo. for 24 mo. or $999 before trade-in</span>
+      <ul className="links">
+        <li><button className='button'>Buy</button></li>
+        <li><a className='link'>Learn more</a></li>
+      </ul>
+      <img className='iphone-img' src={HoldingIphone} alt="iPhone" />
+    </div>
+  )
+}
+
+export default Jumbotron;
